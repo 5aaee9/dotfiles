53f0b27ec46c1cc8120285277b5efee52e418a97
Indexyz <jiduye@gmail.com>
2018-07-25 10:27:49 +0800

    feat: use nvm offical install to install nvm

--- index.sh
+++ index.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! -d "~/.nvm" ]; then
-    git clone https://github.com/creationix/nvm.git ~/.nvm
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 fi
 
 if [ ! -f "~/.config/fish/config.fish" ]; then
