c206fe4726bc6bbdd1f8cd39376275b27e3aafbd
Indexyz <jiduye@gmail.com>
2018-08-14 15:25:18 +0800

    feat: add message

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -2,5 +2,15 @@ function fish_greeting -d "What's up, fish?"
   set_color $fish_color_autosuggestion
   uname -nmsr
   uptime
+  echo -n "
+  _____           _                       ______
+ |_   _|         | |                     |  ____|
+   | |  _ __   __| | _____  ___   _ ____ | |__   _ ____   __
+   | | | '_ \ / _` |/ _ \ \/ / | | |_  / |  __| | '_ \ \ / /
+  _| |_| | | | (_| |  __/>  <| |_| |/ /  | |____| | | \ V /
+ |_____|_| |_|\__,_|\___/_/\_\\__, /___| |______|_| |_|\_/
+                               __/ |
+                              |___/
+"
   set_color normal
 end
