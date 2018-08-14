9b6e7a80b2629542850951bf6ed61f39941d29d3
Indexyz <jiduye@gmail.com>
2018-08-14 15:36:12 +0800

    feat: update

--- arch.sh
+++ arch.sh
@@ -3,5 +3,5 @@ set -e
 
 yes | pacman -S curl fish git
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-fisher Indexyz/dotfiles
+fish -c "fisher Indexyz/dotfiles"
 chsh -s /usr/bin/fish
--- centos.sh
+++ centos.sh
@@ -5,5 +5,5 @@ yum install -y git curl
 yum install -y https://download.opensuse.org/repositories/shells:/fish:/release:/2/CentOS_7/x86_64/fish-2.7.1-1.2.x86_64.rpm
 
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-fisher Indexyz/dotfiles
+fish -c "fisher Indexyz/dotfiles"
 chsh -s /usr/bin/fish
