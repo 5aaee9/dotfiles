a28b3002bb251a5be0934e696391af07955bef22
Indexyz <jiduye@gmail.com>
2020-07-26 23:09:32 +0800

    feat: add gpg.fish

---
+++ macos/gpg.fish
@@ -0,0 +1,18 @@
+
+# Before use, use
+# gpg --import Indexyz.gpg
+# To import private keys
+
+# Install GPG softwares
+brew install gnupg pinentry-mac
+brew cask install gpg-suite
+
+# Update config
+echo "no-tty
+use-agent" > ~/.gnupg/gpg.conf
+
+# Set git use gpg key
+git config --global gpg.program (which gpg)
+
+git config --global commit.gpgsign true
+git config --global user.signingkey 16B8421AAEC1A147
