911e98803290fb57707678cb893a8e18880d33c8
Indexyz <jiduye@gmail.com>
2020-11-15 16:25:22 +0800

    feat: remove cloud-init

--- nixos/profile/vm.nix
+++ nixos/profile/vm.nix
@@ -9,47 +9,6 @@
     # QEMU Guest agent
     services.qemuGuest.enable = true;
 
-    # Cloud-Init
-    services.cloud-init.enable = true;
-    services.cloud-init.config = ''
-        system_info:
-            distro: nixos
-        users:
-            - root
-
-        disable_root: false
-        preserve_hostname: false
-        cloud_init_modules:
-            - migrator
-            - seed_random
-            - bootcmd
-            - write-files
-            - growpart
-            - resizefs
-
-        cloud_config_modules:
-            - disk_setup
-            - mounts
-            - set-passwords
-            - runcmd
-
-        cloud_final_modules:
-            - rightscale_userdata
-            - scripts-vendor
-            - scripts-per-once
-            - scripts-per-boot
-            - scripts-per-instance
-            - scripts-user
-            - ssh-authkey-fingerprints
-            - keys-to-console
-            - final-message
-            - power-state-change
-    '';
-
-    environment.etc."cloud/cloud.cfg.d/99_datastore.cfg".text = ''
-        datasource:
-            ConfigDrive: {}
-            None: {}
-        datasource_list: [ NoCloud, ConfigDrive ]
-    '';
+    # Grow root part
+    boot.growPartition = true;
 }
