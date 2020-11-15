e0e25d5a60a8b4e9bd3f80d2e27539c3bb821429
Indexyz <jiduye@gmail.com>
2020-11-15 14:29:59 +0800

    feat: add vm profile

--- nixos/profile/vm.nix
+++ nixos/profile/vm.nix
@@ -4,5 +4,11 @@
     imports = [
         ../softwares/ssh.nix
     ];
+
+    # QEMU Guest agent
     services.qemuGuest.enable = true;
+
+    # Cloud-Init
+    services.cloud-init.enable = true;
+    services.cloud-init.ext4.enable = true;
 }
