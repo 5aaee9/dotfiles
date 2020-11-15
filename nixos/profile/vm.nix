cdce4feb482f6d8be8659e8c73dca6d772843bd2
Indexyz <jiduye@gmail.com>
2020-11-15 15:58:29 +0800

    feat: update cloud-init config

--- nixos/profile/vm.nix
+++ nixos/profile/vm.nix
@@ -11,6 +11,41 @@
 
     # Cloud-Init
     services.cloud-init.enable = true;
+    services.cloud-init.config = ''
+        system_info:
+            distro: nixos
+        users:
+            - root
+
+        disable_root: false
+        preserve_hostname: false
+        cloud_init_modules:
+            - migrator
+            - seed_random
+            - bootcmd
+            - write-files
+            - growpart
+            - resizefs
+
+        cloud_config_modules:
+            - disk_setup
+            - mounts
+            - set-passwords
+            - runcmd
+
+        cloud_final_modules:
+            - rightscale_userdata
+            - scripts-vendor
+            - scripts-per-once
+            - scripts-per-boot
+            - scripts-per-instance
+            - scripts-user
+            - ssh-authkey-fingerprints
+            - keys-to-console
+            - final-message
+            - power-state-change
+    '';
+
     environment.etc."cloud/cloud.cfg.d/99_datastore.cfg".text = ''
         datasource:
             ConfigDrive: {}
