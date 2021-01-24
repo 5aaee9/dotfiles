eb475f7fc181088326c7238a24e90fe7eb878ff9
Indexyz <jiduye@gmail.com>
2021-01-25 05:34:34 +0800

    feat: add ssh auth support

--- macos/gpg.fish
+++ macos/gpg.fish
@@ -6,9 +6,16 @@
 # Install GPG softwares
 brew install gnupg pinentry-mac gpg-suite
 
-# Update config
-echo "no-tty
-use-agent" > ~/.gnupg/gpg.conf
+echo "default-cache-ttl 600
+max-cache-ttl 7200
+pinentry-program /usr/local/bin/pinentry-mac
+enable-ssh-support" > ~/.gnupg/gpg-agent.conf
+
+echo "# Start or re-use a gpg-agent.
+gpgconf --launch gpg-agent
+
+# Ensure that GPG Agent is used as the SSH agent
+set -xg SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh" > ~/.config/fish/conf.d/gpg-card.fish
 
 # Set git use gpg key
 git config --global gpg.program (which gpg)
